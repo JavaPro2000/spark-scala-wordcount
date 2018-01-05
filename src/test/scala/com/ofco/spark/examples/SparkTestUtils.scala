23f8a80a74770fb11cc3f3ce0febd4b06d5c3214
Oswaldo Fuenmayor <JavaPro2000@gmail.com>
2018-01-05 15:37:24 -0500

    Added windows support

--- src/test/scala/com/ofco/spark/examples/SparkTestUtils.scala
+++ src/test/scala/com/ofco/spark/examples/SparkTestUtils.scala
@@ -1,5 +1,9 @@
 package com.ofco.spark.examples
 
+import org.apache.commons.exec.CommandLine
+import org.apache.commons.lang3.SystemUtils
+import java.io.File
+
 object SparkTestUtils {
   def initTestEnv() {
     val user_dir = System.getProperty("user.dir").replace('\\', '/')
@@ -10,5 +14,28 @@ object SparkTestUtils {
     sys.props.put("spark.sql.warehouse.dir", spark_hive_warehouse_dir)
     sys.props.put("spark.local.dir", user_dir + "/tmp/spark/scratch")
     sys.props.put("spark.master", "local")
+
+    initTmpFolder(user_dir)
+  }
+
+  import org.apache.commons.exec.DefaultExecutor
+  import org.apache.commons.io.FileUtils
+
+  private def initTmpFolder(user_dir: String): Unit = {
+    try {
+      val tmp_folder = user_dir + "/tmp"
+      val file = new File(tmp_folder)
+      if (file.exists) FileUtils.deleteDirectory(file)
+      file.mkdir
+      var line = "chmod -R 777 " + tmp_folder
+      val IS_WINDOWS = SystemUtils.IS_OS_WINDOWS
+      if (IS_WINDOWS) line = user_dir + "/hadoop/bin/winutils.exe " + line
+      val cmd_line = CommandLine.parse(line)
+      val executor = new DefaultExecutor
+      val exit_value = executor.execute(cmd_line)
+      if (exit_value != 0) throw new Exception("Error while executing chmod on tmp folder => " + tmp_folder)
+    } catch {
+      case e: Exception => e.printStackTrace()
+    }
   }
 }
