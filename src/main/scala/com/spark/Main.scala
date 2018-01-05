0559e35cf4470ba9cb56a619ea097c9e3873084c
Oswaldo Fuenmayor <JavaPro2000@gmail.com>
2018-01-05 12:27:41 -0500

    Added gitignore file

---
+++ src/main/scala/com/spark/Main.scala
@@ -0,0 +1,15 @@
+package com.spark
+
+/**
+ * @author Oswaldo Fuenmayor
+ */
+object Main {
+
+  def foo(x: Array[String]) = x.foldLeft("")((a, b) => a + b)
+
+  def main(args: Array[String]) {
+    println("Hello World!")
+    println("concat arguments = " + foo(args))
+  }
+
+}
