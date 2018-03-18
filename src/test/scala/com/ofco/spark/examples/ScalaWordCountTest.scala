50228076d8c77f015dbaa4239974f4d766857ada
Oswaldo Fuenmayor <JavaPro2000@gmail.com>
2018-03-18 11:17:23 -0400

    Formatting changes

--- src/test/scala/com/ofco/spark/examples/ScalaWordCountTest.scala
+++ src/test/scala/com/ofco/spark/examples/ScalaWordCountTest.scala
@@ -5,7 +5,7 @@ import org.scalatest.{BeforeAndAfter, FunSuite}
 class ScalaWordCountTest extends FunSuite with BeforeAndAfter {
 
   test("testMain") {
-    val input_file = "src/test/resources/data/data.txt"
+    val input_file = "data/data.txt"
     val output_dir = "tmp/output"
 
     ScalaWordCount.main(Array(input_file, output_dir))
--- src/test/scala/com/ofco/spark/examples/SparkTestUtils.scala
+++ src/test/scala/com/ofco/spark/examples/SparkTestUtils.scala
@@ -14,27 +14,23 @@ object SparkTestUtils {
     sys.props.put("hadoop.home.dir", user_dir + "/hadoop")
     sys.props.put("hive.exec.scratchdir", user_dir + "/tmp/hive/scratch")
     sys.props.put("spark.sql.warehouse.dir", spark_hive_warehouse_dir)
-    sys.props.put("spark.local.dir", user_dir + "/tmp/spark/scratch")
-    sys.props.put("spark.master", "local")
+    sys.props.put("spark.local.dir", user_dir + "/tmp/spark/tmp")
+    sys.props.put("spark.master", "local[*]")
 
     initTmpFolder(user_dir)
   }
 
   private def initTmpFolder(user_dir: String): Unit = {
-    try {
-      val tmp_folder = user_dir + "/tmp"
-      val file = new File(tmp_folder)
-      if (file.exists) FileUtils.deleteDirectory(file)
-      file.mkdir
-      var line = "chmod -R 777 " + tmp_folder
-      val IS_WINDOWS = SystemUtils.IS_OS_WINDOWS
-      if (IS_WINDOWS) line = user_dir + "/hadoop/bin/winutils.exe " + line
-      val cmd_line = CommandLine.parse(line)
-      val executor = new DefaultExecutor
-      val exit_value = executor.execute(cmd_line)
-      if (exit_value != 0) throw new Exception("Error while executing chmod on tmp folder => " + tmp_folder)
-    } catch {
-      case e: Exception => e.printStackTrace()
-    }
+    val tmp_folder = user_dir + "/tmp"
+    val file = new File(tmp_folder)
+    if (file.exists) FileUtils.deleteDirectory(file)
+    file.mkdir
+    var line = "chmod -R 777 " + tmp_folder
+    val IS_WINDOWS = SystemUtils.IS_OS_WINDOWS
+    if (IS_WINDOWS) line = user_dir + "/hadoop/bin/winutils.exe " + line
+    val cmd_line = CommandLine.parse(line)
+    val executor = new DefaultExecutor
+    val exit_value = executor.execute(cmd_line)
+    if (exit_value != 0) throw new Exception("Error while executing chmod on tmp folder => " + tmp_folder)
   }
 }
